c6a39bb80af9ea25a508c6055c38a5ba0ab6f594
taomylife521/ <514935323@qq.com>
2020-08-23 21:46:40 +0800

    Signed-off-by: first commit

---
+++ js/public_share_code.js
@@ -0,0 +1,11 @@
+﻿$(function(){
+        /*公共部分
+         * 导航栏
+         * footer CopyRight
+         */
+    $(".top-main").load("../plus/header.html");
+    $(".touch-top-home").load("../plus/touchheader.html");
+    $(".footer").load("../plus/footer.html");
+    $(".bottom").load("../plus/bottom.html");
+
+});
---
+++ js/public_share_innercode.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    /*公共部分
+     * 导航栏
+     * footer CopyRight
+     */
+    $(".top-main").load("header.html");
+    $(".touch-top-home").load("touchheader.html")
+    $(".footer").load("footer.html");
+    $(".bottom").load("bottom.html");
+});
