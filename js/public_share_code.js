c6f133669d67f0755ed9c948156d005d7331187d
taomylife521/ <514935323@qq.com>
2020-08-28 10:07:35 +0800

    Signed-off-by: 修改路径

--- js/public_share_code.js
+++ js/public_share_code.js
@@ -3,9 +3,9 @@
          * 导航栏
          * footer CopyRight
          */
-    $(".top-main").load("../plus/header.html");
-    $(".touch-top-home").load("../plus/touchheader.html");
-    $(".footer").load("../plus/footer.html");
-    $(".bottom").load("../plus/bottom.html");
+    $(".top-main").load("plus/header.html");
+    $(".touch-top-home").load("plus/touchheader.html");
+    $(".footer").load("plus/footer.html");
+    $(".bottom").load("plus/bottom.html");
 
 });
